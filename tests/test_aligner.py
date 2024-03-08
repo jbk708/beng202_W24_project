44a1b3c830b319a8fed9d7cb7ff46e5ae7e6f074
Jonathan Kirkland <jkirkland@eighteenfiftynine.com>
2024-03-07 17:57:57 -0800

    added position score test

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -3,18 +3,50 @@ import os
 import pytest
 from mlst_aligner.aligner import positional_alignment
 
-@pytest.mark.parametrize("match_reward, mismatch_penalty, indel_penalty, s, t, expected_score, expected_aligned_s, expected_aligned_t", [
-    (3, 3, 1, "AGC", "ATC", 4, "A-GC", "AT-C"),
-    (1, 1, 1, "TAACG", "ACGTG", 3, "ACG", "ACG"),
-    (3, 2, 1, "CAGAGATGGCCG", "ACG", 6, "CG", "CG"),
-    (2, 3, 1, "CTT", "AGCATAAAGCATT", 5, "C-TT", "CATT"),
-])
-def test_positional_alignment(match_reward, mismatch_penalty, indel_penalty, s, t, expected_score, expected_aligned_s, expected_aligned_t):
+
+@pytest.mark.parametrize(
+    "match_reward, mismatch_penalty, indel_penalty, s, t, expected_score, expected_aligned_s, expected_aligned_t, expected_position_score", [
+        (3, 3, 1, "AGC", "ATC", 4, "A-GC", "AT-C", {
+            1: 1,
+            2: 0,
+            3: 4
+        }),
+        (1, 1, 1, "TAACG", "ACGTG", 3, "ACG", "ACG", {
+            1: 0,
+            2: 1,
+            3: 3,
+            4: 2,
+            5: 1
+        }),
+        (3, 2, 1, "CAGAGATGGCCG", "ACG", 6, "CG", "CG", {
+            1: 0,
+            2: 2,
+            3: 6
+        }),
+        (2, 3, 1, "CTT", "AGCATAAAGCATT", 5, "C-TT", "CATT", {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 2,
+            6: 1,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0,
+            12: 2,
+            13: 5
+        }),
+    ])
+def test_positional_alignment(match_reward, mismatch_penalty, indel_penalty, s, t, expected_score, expected_aligned_s,
+                              expected_aligned_t, expected_position_score):
     # Run the alignment
-    score, aligned_s, aligned_t, scores_at_positions = positional_alignment(match_reward, mismatch_penalty, indel_penalty, s, t)
+    score, aligned_s, aligned_t, scores_at_positions = positional_alignment(match_reward, mismatch_penalty, indel_penalty, s,
+                                                                            t)
 
     # Assert that the returned score and alignments are as expected
     assert score == expected_score
     assert aligned_s == expected_aligned_s
     assert aligned_t == expected_aligned_t
-    print(scores_at_positions)
+    assert scores_at_positions == expected_position_score
